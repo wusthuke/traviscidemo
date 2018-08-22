03dfde39ac2b14be41c68b1842c2d7858f07725c
科豆 <kedou.hk@antfin.com>
2018-08-22 14:51:36 +0800

    add user.java

---
+++ src/main/java/com/huke/demo/User.java
@@ -0,0 +1,22 @@
+/**
+ * Alipay.com Inc. Copyright (c) 2004-2018 All Rights Reserved.
+ */
+package com.huke.demo;
+
+/**
+ *
+ * @author huke
+ * @version $Id: User.java, v 0.1 2018年08月22日 下午2:51 huke Exp $
+ */
+public class User {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
